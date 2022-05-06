a882b546171c460c08f12cef1fff54c2b1c66e12
GabrielPurper <104388283+GabrielPurper@users.noreply.github.com>
2022-05-06 15:40:32 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Aula_9
+# project_c9_pro_new
--- sketch.js
+++ sketch.js
@@ -1,31 +1,41 @@
-var box;
 
 function setup() {
   createCanvas(400,400);
-
+  background(51);
   box = createSprite(200,200,30,30);
+
 }
 
 function draw() 
 {
-  background(0);
 
-  if(keyIsDown(RIGHT_ARROW))
-{
-  box.position.x = box.position.x +5;
-}
-if(keyIsDown(LEFT_ARROW))
-{
-  box.position.x = box.position.x -5;
-}
-if(keyIsDown(UP_ARROW))
-{
-  box.position.y = box.position.y -5;
-}
-if(keyIsDown(DOWN_ARROW))
-{
-  box.position.y = box.position.y +5;
-}
-drawSprites();
+  // escreva o código para alterar a cor de fundo 
+  background("black");
+  // para vermelho quando a seta para direita (RIGHT_ARROW) for pressionada
+  if(keyIsDown((RIGHT_ARROW)))
+  {
+    background("red");
+  }
+  
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+
+  
+  drawSprites();
 }
 
